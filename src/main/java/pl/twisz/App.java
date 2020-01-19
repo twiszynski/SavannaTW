720944f7b565bd0522c29e81d0cc2a0ca5efcc9c
twish <t.wiszynski@gmail.com>
2020-01-19 19:14:32 +0100

    simulator checks

--- src/main/java/pl/twisz/App.java
+++ src/main/java/pl/twisz/App.java
@@ -5,10 +5,10 @@ public class App {
 
         // creating savanna board with plants
         Savanna s1 = new Savanna(5, 7, 17);
-        Savanna s2 = new Savanna(3, 3, 4);
+        Savanna s2 = new Savanna(10, 13, 70);
         // populating savanna with animals
         s1.populateSavanna(3, 16);
-        s2.populateSavanna(0, 5);
+        s2.populateSavanna(5, 60);
         // using SavannaSimulator object for two modes of simulation
         SavannaSimulator sim = new SavannaSimulator();
         sim.simulateByGivenDays(s1, 17);
